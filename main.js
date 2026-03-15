980702d2339082b606c8d74a28872bcc0057cfe4
dannyccash-web <dannyccash@gmail.com>
2026-03-14 23:42:36 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -44,8 +44,8 @@ function createHeroAnimations(scene, heroKey) {
   const animations = [
     { key: `${heroKey}-walk-left`, sheet: `${heroKey}-walk`, row: 1, start: 0, end: 8, rate: 10, repeat: -1 },
     { key: `${heroKey}-walk-right`, sheet: `${heroKey}-walk`, row: 3, start: 0, end: 8, rate: 10, repeat: -1 },
-    { key: `${heroKey}-idle-left`, sheet: `${heroKey}-idle`, row: 1, start: 0, end: 1, rate: 3, repeat: -1 },
-    { key: `${heroKey}-idle-right`, sheet: `${heroKey}-idle`, row: 3, start: 0, end: 1, rate: 3, repeat: -1 },
+    { key: `${heroKey}-idle-left`, sheet: `${heroKey}-idle`, row: 1, start: 0, end: 0, rate: 1, repeat: -1 },
+    { key: `${heroKey}-idle-right`, sheet: `${heroKey}-idle`, row: 3, start: 0, end: 0, rate: 1, repeat: -1 },
     { key: `${heroKey}-jump-left`, sheet: `${heroKey}-jump`, row: 1, start: 0, end: 3, rate: 12, repeat: 0 },
     { key: `${heroKey}-jump-right`, sheet: `${heroKey}-jump`, row: 3, start: 0, end: 3, rate: 12, repeat: 0 },
   ];
@@ -567,7 +567,7 @@ class PrototypeScene extends Phaser.Scene {
       .setScale(0.80)
       .setDepth(this.propDepth);
 
-    this.onionPatch = this.add.image(2790, 772, 'onionPatch')
+    this.onionPatch = this.add.image(2790, 770, 'onionPatch')
       .setOrigin(0.5, 1)
       .setScale(0.34)
       .setDepth(this.propDepth);
@@ -872,7 +872,8 @@ class PrototypeScene extends Phaser.Scene {
     this.portraitSceneBg.setTileScale(this.bgScale || 1, this.bgScale || 1);
     this.dialogueOverlay.add(this.portraitSceneBg);
 
-    this.portraitMaskGraphics = this.make.graphics({ x: 0, y: 0, add: false });
+    this.portraitMaskGraphics = this.add.graphics();
+    this.portraitMaskGraphics.setScrollFactor(0).setDepth(229).setVisible(false);
     this.portraitMaskGraphics.fillStyle(0xffffff, 1);
     this.portraitMaskGraphics.fillRect(
       this.dialoguePortraitRect.left,
@@ -883,10 +884,11 @@ class PrototypeScene extends Phaser.Scene {
     this.portraitMask = this.portraitMaskGraphics.createGeometryMask();
     this.portraitSceneBg.setMask(this.portraitMask);
 
-    this.npcPortrait = this.add.sprite(portraitX, this.dialoguePortraitRect.bottom, 'forestLady-idle', 26)
+    this.npcPortrait = this.add.sprite(portraitX, this.dialoguePortraitRect.bottom + 8, 'forestLady-idle', 26)
       .setOrigin(0.5, 1)
       .setScale(5.1)
-      .setMask(this.portraitMask);
+      .setMask(this.portraitMask)
+      .setVisible(true);
     this.dialogueOverlay.add(this.npcPortrait);
 
     this.dialogueSpeakerText = this.add.text(textX, contentTop + 10, FOREST_LADY.name, {
@@ -1058,6 +1060,8 @@ class PrototypeScene extends Phaser.Scene {
     this.npc.setVelocityX(0);
     this.player.anims.play(`${this.heroKey}-idle-${this.facing}`, true);
     this.npc.anims.play(this.npcFacing === 'left' ? 'forestLady-idle-left' : 'forestLady-idle-right', true);
+    this.portraitSceneBg?.setVisible(true);
+    this.npcPortrait?.setVisible(true);
     this.syncDialoguePortraitBackground();
     this.startDialogueSequence(sequence);
   }
